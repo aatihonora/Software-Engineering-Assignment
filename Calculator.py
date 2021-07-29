f1f5490a679f1bb59927595a9aea620fa431b4da
GhullamSarwarKhushk <88105144+GhullamSarwarKhushk@users.noreply.github.com>
2021-07-29 15:44:41 +0500

    Completed the Calculator

--- Calculator.py
+++ Calculator.py
@@ -26,15 +26,25 @@ def exponents(x, y):
     return x ** y
 
 
+def percentage(x, y):
+    quotient = x / y
+    return quotient * 100
+
+
 statement1 = "Do you want to continue the program y/n"
 statement2 = "Terminating..."
 statement3 = "Invalid answer"
+statement4 = "Select operation."
 print("Select operation.")
 print("1.Add  2.Subtract  3.Multiply  4.Divide  5.Exponents  6.Percentage")
 print("7.Sin0  8.Cos0  9.Tan0  10.Square Root  11.Logarithm  12.Anti-Logarithm")
 print("13.Quit/Terminate")
 
 while True:
+    print("Select operation.")
+    print("1.Add  2.Subtract  3.Multiply  4.Divide  5.Exponents  6.Percentage")
+    print("7.Sin0  8.Cos0  9.Tan0  10.Square Root  11.Logarithm  12.Anti-Logarithm")
+    print("13.Quit/Terminate")
     choice = input("Enter the choice (1 to 13): ")
 
     if choice in ('1', '2', '3', '4', '5', '6'):
@@ -100,10 +110,19 @@ while True:
                 break
             else:
                 print(statement3)
-        
+
         elif choice == '6':
-            #Percentage
-        
+            print(f"Percentage of {num1 / num2} =", percentage(num1, num2))
+            print(statement1)
+            answer = input()
+            if answer.lower() == 'y':
+                continue
+            elif answer.lower() == 'n':
+                print(statement2)
+                break
+            else:
+                print(statement3)
+
     elif choice in ('7', '8', '9'):
         num1 = float(input("Enter the angle: "))
 
@@ -148,7 +167,7 @@ while True:
 
     elif choice in ('10', '11', '12'):
         num1 = float(input("Enter the value: "))
-        
+
         if choice == '10':
             print(f"Square root of {num1} =", math.sqrt(num1))
             print(statement1)
@@ -191,4 +210,3 @@ while True:
 
     else:
         print("Invalid Input")
-
