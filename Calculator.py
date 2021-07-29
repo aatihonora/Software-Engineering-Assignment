bc62e8880e69bcf822c9812e91ae67ff3d9457fb
Behram Akhtar <58216695+aatihonora@users.noreply.github.com>
2021-07-29 14:23:10 +0500

    Added trigonmetry functionality
    Fixed some underlying problems, made program look a bit simpler and added trigonometry functions

--- Calculator.py
+++ Calculator.py
@@ -2,6 +2,10 @@
 
 # I am going to make some basic operation function as I am not very good at python can you make the complex ones (
 # Ghullam Sarwar)
+
+import math
+
+
 def add(x, y):
     return x + y
 
@@ -22,9 +26,13 @@ def exponents(x, y):
     return x ** y
 
 
+statement1 = "Do you want to continue the program y/n"
+statement2 = "Terminating..."
+statement3 = "Invalid Input"
 print("Select operation.")
 print("1.Add  2.Subtract  3.Multiply  4.Divide  5.Exponents")
-print("6.Quit/Terminate")
+print("6.SinO  7.CosO  8.Tan0  9.Square Root  10.Logarithm ")
+print("11.Quit/Terminate")
 
 while True:
     choice = input("Enter choice: ")
@@ -35,73 +43,115 @@ while True:
 
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
-            print("Do you want to continue the program y/n")
+            print(statement1)
             answer = input()
             if answer.lower() == 'y':
                 continue
             elif answer.lower() == 'n':
-                print("Terminating...")
+                print(statement2)
                 break
             else:
-                print("Invalid answer")
+                print(statement3)
                 break
 
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
-            print("Do you want to continue the program y/n")
+            print(statement1)
             answer = input()
             if answer.lower() == 'y':
                 continue
             elif answer.lower() == 'n':
-                print("Terminating...")
+                print(statement2)
                 break
             else:
-                print("Invalid answer")
+                print(statement3)
                 break
 
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
-            print("Do you want to continue the program y/n")
+            print(statement1)
             answer = input()
             if answer.lower() == 'y':
                 continue
             elif answer.lower() == 'n':
-                print("Terminating...")
+                print(statement2)
                 break
             else:
-                print("Invalid answer")
+                print(statement3)
                 break
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
-            print("Do you want to continue the program y/n")
+            print(statement1)
             answer = input()
             if answer.lower() == 'y':
                 continue
             elif answer.lower() == 'n':
-                print("Terminating...")
+                print(statement2)
                 break
             else:
-                print("Invalid answer")
+                print(statement3)
                 break
 
         elif choice == '5':
             print(num1, "^", num2, "=", exponents(num1, num2))
-            print("Do you want to continue the program y/n")
+            print(statement1)
             answer = input()
             if answer.lower() == 'y':
                 continue
             elif answer.lower() == 'n':
-                print("Terminating...")
+                print(statement2)
                 break
             else:
-                print("Invalid answer")
+                print(statement3)
+                break
+    elif choice in ('6', '7', '8'):
+        num1 = float(input("Enter the angle: "))
+
+        if choice == '6':
+            degree = num1 * 0.0174532925
+            print(f"Sin0 {num1}° =", round(math.sin(degree), 4))
+            print(statement1)
+            answer = input()
+            if answer.lower() == 'y':
+                continue
+            elif answer.lower() == 'n':
+                print(statement2)
+                break
+            else:
+                print(statement3)
+                break
+        elif choice == '7':
+            degree = num1 * 0.0174532925
+            print(f"Cos0 {num1}° =", round(math.cos(degree), 4))
+            print(statement1)
+            answer = input()
+            if answer.lower() == 'y':
+                continue
+            elif answer.lower() == 'n':
+                print(statement2)
+                break
+            else:
+                print(statement3)
                 break
 
-    elif choice == '6':
-        print("Terminating...")
+        elif choice == '8':
+            degree = num1 * 0.0174532925
+            print(f"Tan0 {num1}° =", round(math.tan(degree), 4))
+            print(statement1)
+            answer = input()
+            if answer.lower() == 'y':
+                continue
+            elif answer.lower() == 'n':
+                print(statement2)
+                break
+            else:
+                print(statement3)
+                break
+
+    elif choice == '11':
+        print(statement2)
         break
 
     else:
-        print("Invalid Input")
-        continue
+        print(statement3)
